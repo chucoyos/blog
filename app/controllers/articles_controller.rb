e528feb03b97cfc5a0dbe1d01cb2aa2f58c33f19
chucoyos <manzanillo91@hotmail.com>
2020-06-01 03:10:07 -0500

    Delete articles added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,11 +8,11 @@ class  ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
-
+    #GET /articles/new
     def new
       @article = Article.new   
     end
-
+    #POS /articles/new
     def create
       @article = Article.new({title: params[:article] [:title],
       body: params[:article] [:body]})
@@ -23,5 +23,15 @@ class  ArticlesController < ApplicationController
         render :new
       end
       
+    end
+
+    def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+      redirect_to articles_path
+    end
+
+    def update
+      #@article.update_atributes({title: 'Nuevo Titulo'})
     end
 end
